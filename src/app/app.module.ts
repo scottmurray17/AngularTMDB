764a5fc586be05d560a68042ca2d7c87bce4f213
smurray117 <smurray1@stevens.edu>
2023-04-02 12:45:39 -0400

    Remove template and add placeholder components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieComponent } from './movie/movie.component';
+import { TvComponent } from './tv/tv.component';
+import { PersonComponent } from './person/person.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieComponent,
+    TvComponent,
+    PersonComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
